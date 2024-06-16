e31aa6ab17688661ac91658202ac02c717956964
Sahilbalhra <sahilbalhra0@gmail.com>
2024-06-16 15:59:20 +0530

    added  loading with suspense

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,15 @@
+import { Suspense } from "react";
+import Loading from "@/components/Loading";
 import Banner from "@/app/(home)/components/Banner";
-import BookList from "./components/BookList";
+import BookList from "@/app/(home)/components/BookList";
 
 export default function Home() {
   return (
     <>
       <Banner />
-      <BookList />
+      <Suspense fallback={<Loading />}>
+        <BookList />
+      </Suspense>
     </>
   );
 }
