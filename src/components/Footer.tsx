567127d41efab1d2326ef01b94c78ca491ad6490
Sahilbalhra <sahilbalhra0@gmail.com>
2024-06-16 15:55:59 +0530

    added footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="border-t p-5 text-center">
+      <p>© {new Date().getFullYear()} E-Books All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
