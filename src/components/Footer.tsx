72728880ccbf42f2663bb401426414c00466287a
Sahilbalhra <sahilbalhra0@gmail.com>
2024-07-18 22:41:24 +0530

    some new improvement and added singin page with dependencies upgrades

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="border-t p-5 text-center">
+    <footer className="border-t p-5 text-center absolute bottom-0 w-screen">
       <p>© {new Date().getFullYear()} E-Books All rights reserved.</p>
     </footer>
   );
